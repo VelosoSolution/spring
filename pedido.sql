ee353bc3246831602d1df286a09050abb4d91cd6
Veloso Solution <167025127+VelosoSolution@users.noreply.github.com>
2024-08-11 15:03:03 -0300

    Add files via upload
    Banco de dados mysql

---
+++ pedido.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from pedido
+LIMIT 0, 1000
+
+-- Date: 2024-08-10 23:47
+*/
+INSERT INTO `` (`id`,`codigo_cliente`,`data_cadastro`,`nome_produto`,`numero_controle`,`quantidade`,`valor_total`,`valor_unitario`) VALUES (1,1,'2024-08-10','papel','2',10,100.00,10.00);
+INSERT INTO `` (`id`,`codigo_cliente`,`data_cadastro`,`nome_produto`,`numero_controle`,`quantidade`,`valor_total`,`valor_unitario`) VALUES (2,1,'2024-08-10','papel2','3',10,90.00,10.00);
+INSERT INTO `` (`id`,`codigo_cliente`,`data_cadastro`,`nome_produto`,`numero_controle`,`quantidade`,`valor_total`,`valor_unitario`) VALUES (3,1,'2024-08-10','papel3','4',15,135.00,10.00);
+INSERT INTO `` (`id`,`codigo_cliente`,`data_cadastro`,`nome_produto`,`numero_controle`,`quantidade`,`valor_total`,`valor_unitario`) VALUES (4,1,'2024-08-10','papel3','5',10,90.00,10.00);
+INSERT INTO `` (`id`,`codigo_cliente`,`data_cadastro`,`nome_produto`,`numero_controle`,`quantidade`,`valor_total`,`valor_unitario`) VALUES (5,1,'2024-08-10','papel33','6',10,90.00,10.00);
